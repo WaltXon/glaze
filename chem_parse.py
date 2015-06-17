2878be7362b1867f6a99950ba775e4db8a3db2da
WaltXon <walt.nixon@gmail.com>
2015-06-17 09:24:12 -0500

    fixed parser, made it a function

--- chem_parse.py
+++ chem_parse.py
@@ -5,37 +5,37 @@ Created on Mon Jun 15 13:08:28 2015
 @author: waltn_000
 """
 
-from pyparsing import Word, Optional, OneOrMore, Group, ParseException, ZeroOrMore, Literal
+from pyparsing import Word, Optional, OneOrMore, Group, ParseException, Literal
 import pandas as pd
 import os
-import itertools
-
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-
-df = pd.read_table(os.path.join(BASE_DIR, 'elements.csv'),sep=',',  header=0, skipinitialspace=True)
 
 
-caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lowers = caps.lower()
-digits = "0123456789"
+def atomic_weight(item):
+    BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+    df = pd.read_table(os.path.join(BASE_DIR, 'elements.csv'),sep=',',  header=0, skipinitialspace=True)
+    
+    caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowers = caps.lower()
+    digits = "0123456789"
+    
+    element = Word( caps, lowers ) 
+    elementRef = Group( element + Optional( Word( digits ), default="1" ) )
+    formula = OneOrMore( elementRef + Optional( Literal('.')))
+    compound = OneOrMore(formula)
+    
 
-element = Word( caps, lowers ) 
-elementRef = Group( element + Optional( Word( digits ), default="1" ) )
-formula = OneOrMore( elementRef + Optional( Literal('.')))
-compound = OneOrMore(formula)
-
-tests = [ "Al2O3", "SiO2", 'Al2O3.SiO2' ]
-for t in tests:
     try:
-        results = compound.parseString( t ).asList()
+        results = compound.parseString( item ).asList()
         if '.' in results:
-            data = [list(v) for k,v in itertools.groupby(results, key='.')]
+            data = [list(k) for k in results if k != '.']
         else:
-            results = list(results)
-        print t,"->", results,
+            data = results
+        
     except ParseException, pe:
         print pe
     else:
-        #wt = sum([float(df[df['Symbol']=='{}'.format(elem)].Atomic_Weight.iloc[0]) * int(qty) for elem,qty in results])
-        #print ("{}".format(wt))
-        pass
+        wt = sum([float(df[df['Symbol']=='{}'.format(elem)].Atomic_Weight.iloc[0]) * int(qty) for elem,qty in data])
+    print("{} -> {} ({})".format(item, data, wt))
+
+
+#atomic_weight('Al2O3')
