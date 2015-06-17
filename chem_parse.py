75c92736db3753c94ce46caac7ced6ea2eedee03
WaltXon <walt.nixon@gmail.com>
2015-06-16 22:07:09 -0500

    messing with parser, broke it

--- chem_parse.py
+++ chem_parse.py
@@ -5,10 +5,10 @@ Created on Mon Jun 15 13:08:28 2015
 @author: waltn_000
 """
 
-from pyparsing import Word, Optional, OneOrMore, Group, ParseException
+from pyparsing import Word, Optional, OneOrMore, Group, ParseException, ZeroOrMore, Literal
 import pandas as pd
 import os
-import string
+import itertools
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -19,18 +19,23 @@ caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lowers = caps.lower()
 digits = "0123456789"
 
-element = Word( caps, lowers )
+element = Word( caps, lowers ) 
 elementRef = Group( element + Optional( Word( digits ), default="1" ) )
-formula = OneOrMore( elementRef )
+formula = OneOrMore( elementRef + Optional( Literal('.')))
+compound = OneOrMore(formula)
 
-tests = [ "Al2O3", "SiO2" ]
+tests = [ "Al2O3", "SiO2", 'Al2O3.SiO2' ]
 for t in tests:
     try:
-        results = formula.parseString( t )
+        results = compound.parseString( t ).asList()
+        if '.' in results:
+            data = [list(v) for k,v in itertools.groupby(results, key='.')]
+        else:
+            results = list(results)
         print t,"->", results,
     except ParseException, pe:
         print pe
     else:
-        wt = sum([float(df[df['Symbol']=='{}'.format(elem)].Atomic_Weight.iloc[0]) * int(qty) for elem,qty in results])
-        print ("{}".format(wt))
-
+        #wt = sum([float(df[df['Symbol']=='{}'.format(elem)].Atomic_Weight.iloc[0]) * int(qty) for elem,qty in results])
+        #print ("{}".format(wt))
+        pass
