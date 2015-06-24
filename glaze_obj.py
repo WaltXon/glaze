8793409684bbe2306be3c41407eaf33c14add54c
WaltXon <walt.nixon@gmail.com>
2015-06-23 22:38:54 -0500

    materials list

--- glaze_obj.py
+++ glaze_obj.py
@@ -4,6 +4,7 @@ Created on Tue Jun 16 12:29:08 2015
 
 @author: Walt
 '''
+from chem_parse import atomic_weight
 
 class Glaze:
     
@@ -14,9 +15,42 @@ class Glaze:
             self.materials = receipe[0]['materials']
             self.additives = receipe[0]['additives']
         
-    def material_list(self):
-        pass
-    
+    def material_list(self, total_weight):
+        if self.type == 'batch':
+            total = sum(self.materials.values())
+            
+            percents ={}
+            for k,v in self.materials.iteritems():
+                percents[k] = float(v)/total                                    
+            
+            weights = {}
+            for k,v in percents.iteritems():
+                weights[k] = v*float(total_weight)
+            
+        print('Batch Materials')
+        for k,v in self.materials.iteritems():
+            print('-- {}: {}'.format(k,v))
+        print('Total = {}'.format(total))
+        print('')
+        print('Percent Materials')
+        for k,v in percents.iteritems():
+            print('-- {}: {}'.format(k,v))
+        print('Total = {}'.format(sum(percents.values())))
+        for k,v in self.additives.iteritems():
+            print('-- {}: {}'.format(k,v))
+        print('')
+        print('Weights Materials')
+        for k,v in weights.iteritems():
+            print('-- {}: {}'.format(k,v))
+        print('Total = {}'.format(sum(weights.values())))
+        
+        if self.additives !={}:
+            print('Additives')
+            for k,v in self.additives.iteritems():
+                weights[k] = [x*float(total_weight)/100.0 for x in v]
+                print('-- {}: {}'.format(k,weights[k]))
+            #print('Total Weight = {}'.format(sum(weights.values())))
+            
     def to_batch(self):
         if self.type == 'batch':
             print 'only for percentage recepies'
@@ -26,3 +60,10 @@ class Glaze:
     def to_percent(self):
         if self.type == 'percent':
             print 'only for batch recepies'
+
+    def to_unity(self):
+        if self.type == 'percent':
+            print 'only for batch recepies'            
+            
+            
+            
